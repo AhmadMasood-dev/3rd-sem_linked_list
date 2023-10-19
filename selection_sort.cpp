8f04cfe1d16cbeef46229c8deb07c8a2e57a1b1f
ZainGondal005 <zaingondal005@gmail.com>
2023-10-20 01:32:37 +0500

    random_number_in_selection_sort

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
 using namespace std;
 class selection_sort
 {
@@ -7,13 +9,6 @@ private:
     int size;
 
 public:
-    selection_sort()
-    {
-    }
-    ~selection_sort()
-    {
-        delete[] array;
-    }
     void setvalues(int n)
     {
         size = n;
@@ -25,15 +20,8 @@ public:
         }
         cout << endl;
     }
-    void print()
-    {
-        for (int i = 0; i < size; i++)
-        {
-            cout << "Value "<<i+1<<" is : " << array[i] << endl;
-        }
-        cout << "----------------------------------\n";
-    }
-    void bubblesort()
+
+    void selection_Sort()
     {
         int min_index, temp, count = 0;
         for (int i = 0; i < size - 1; i++)
@@ -53,25 +41,50 @@ public:
         }
         if (count == 0)
         {
-            throw("array is already sorted");
+            cout << "Array is already sorted" << endl;
         }
+    }
+    void random_number(int n)
+    {
+        size = n;
+        array = new int[size];
+        int num;
+        srand(time(NULL));
+        for (int i = 0; i < size; i++)
+        {
+            num = (rand() % 10 + 1);
+            array[i] = num;
+        }
+    }
+    void print()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << "Value " << i + 1 << " is : " << array[i] << endl;
+        }
+        cout << "----------------------------------\n";
+    }
+    ~selection_sort()
+    {
+        delete[] array;
     }
 };
 int main()
 {
     int n;
-    selection_sort obj_1;
+    selection_sort obj_1, obj_2;
     cout << "Enter a size of array :  ";
     cin >> n;
-    try
-    {
-        obj_1.setvalues(n);
-        obj_1.bubblesort();
-    }
-    catch (const char *msg)
-    {
-        std::cout << msg << std::endl;
-    }
+
+    obj_1.setvalues(n);
+    obj_1.selection_Sort();
+    obj_2.random_number(n);
+    cout << "Array of random number Before sort" << endl;
+    obj_2.print();
+    obj_2.selection_Sort();
+    cout << "Array of random number After sort" << endl;
+    obj_2.print();
+    cout << "Simple Array After sort" << endl;
     obj_1.print();
     return 0;
 }
