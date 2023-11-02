3efaf6ff29b798bf34a24d0ee1f42a36bc8a4bea
ZainGondal005 <zaingondal005@gmail.com>
2023-11-02 21:46:23 +0500

    stack_lec4_brackets and gitIgnore file in stack branch

---
+++ stack_lec4_brackets.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *next;
+};
+class stack
+{
+private:
+    Node *TOP;
+
+public:
+    stack()
+    {
+        TOP = NULL;
+    }
+    void push(const int num)
+    {
+        if (TOP == NULL)
+        {
+
+            TOP = new Node();
+            TOP->data = num;
+            TOP->next = NULL;
+        }
+        else
+        {
+            Node *temp = new Node();
+            temp = TOP;
+            TOP = new Node();
+            TOP->data = num;
+            TOP->next = temp;
+        }
+    }
+    void pop()
+    {
+        Node *temp;
+        temp = TOP->next;
+        delete TOP;
+        TOP = temp;
+    }
+    bool isempty()
+    {
+        if (TOP == NULL)
+            return true;
+        else
+            return false;
+    }
+    void Display()
+    {
+        Node *ptr = TOP;
+        while (ptr != NULL)
+        {
+            std::cout << ptr->data << " -> ";
+            ptr = ptr->next;
+        }
+        std::cout << "\n-----------------------------\n";
+    }
+    int TOP_value(){
+        if (TOP==NULL)
+        {
+            throw ("Staack underflow");
+        }
+        else{
+            return TOP->data;
+        }
+        
+    }
+    ~stack() {}
+};
+
+int main()
+{
+    stack obj_1;
+    obj_1.push(1);
+   cout<<"TOP value is " <<obj_1.TOP_value()<<endl;
+     obj_1.Display();
+}
