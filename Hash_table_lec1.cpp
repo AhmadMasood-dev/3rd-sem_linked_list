b35297ccc8d0d0983f8d63b2e6ab5e4f8d5c5b78
ZainGondal005 <zaingondal005@gmail.com>
2024-01-15 18:32:27 +0500

    hash table lec_1

---
+++ Hash_table_lec1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+template <class T>
+struct Node
+{
+    T data;
+    Node<T> *next;
+};
+
+template <typename T>
+class Hash
+{
+private:
+    T data
+
+        public:
+};
+
+int main()
+{
+    return 0;
+}
