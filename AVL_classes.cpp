bc62db7ab01cf4bc786678aa41573c718bd4f07c
ZainGondal005 <zaingondal005@gmail.com>
2024-01-29 01:22:47 +0500

    tree

--- AVL_classes.cpp
+++ AVL_classes.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 
-class AVLTree {
+class AVLTree
+{
 private:
-    struct Node {
+    struct Node
+    {
         int key;
-        Node* left;
-        Node* right;
+        Node *left;
+        Node *right;
         int height;
     };
 
-    Node* root;
+    Node *root;
 
-    int getHeight(Node* n) {
+    int getHeight(Node *n)
+    {
         if (n == nullptr)
             return 0;
         return n->height;
     }
 
-    Node* createNode(int key) {
-        Node* node = new Node;
+    Node *createNode(int key)
+    {
+        Node *node = new Node;
         node->key = key;
         node->left = nullptr;
         node->right = nullptr;
@@ -26,19 +30,26 @@ private:
         return node;
     }
 
-    int max(int a, int b) {
+    int max(int a, int b)
+    {
         return (a > b) ? a : b;
     }
 
-    int getBalanceFactor(Node* n) {
+    int getBalanceFactor(Node *n)
+    {
         if (n == nullptr)
             return 0;
         return getHeight(n->left) - getHeight(n->right);
     }
 
-    Node* rightRotate(Node* y) {
-        Node* x = y->left;
-        Node* T2 = x->right;
+    Node *rightRotate(Node *y)
+    {
+        Node *x = y->left;
+        Node *T2 = x->right;
+
+        //     y
+        //  x      T3
+        // T1  T2
 
         x->right = y;
         y->left = T2;
@@ -46,12 +57,21 @@ private:
         x->height = max(getHeight(x->right), getHeight(x->left)) + 1;
         y->height = max(getHeight(y->right), getHeight(y->left)) + 1;
 
+        //      x
+        //  T1      y
+        //      T2     T3
+
         return x;
     }
 
-    Node* leftRotate(Node* x) {
-        Node* y = x->right;
-        Node* T2 = y->left;
+    Node *leftRotate(Node *x)
+    {
+        Node *y = x->right;
+        Node *T2 = y->left;
+
+        //      x
+        //  T1      y
+        //      T2     T3
 
         y->left = x;
         x->right = T2;
@@ -59,10 +79,15 @@ private:
         x->height = max(getHeight(x->right), getHeight(x->left)) + 1;
         y->height = max(getHeight(y->right), getHeight(y->left)) + 1;
 
+        //     y
+        //  x      T3
+        // T1  T2
+
         return y;
     }
 
-    Node* insert(Node* node, int key) {
+    Node *insert(Node *node, int key)
+    {
         if (node == nullptr)
             return createNode(key);
 
@@ -78,19 +103,23 @@ private:
             return rightRotate(node);
         if (bf < -1 && key > node->right->key)
             return leftRotate(node);
-        if (bf > 1 && key > node->left->key) {
+        if (bf > 1 && key > node->left->key)
+        {
             node->left = leftRotate(node->left);
             return rightRotate(node);
         }
-        if (bf < -1 && key < node->right->key) {
+        if (bf < -1 && key < node->right->key)
+        {
             node->right = rightRotate(node->right);
             return leftRotate(node);
         }
         return node;
     }
 
-    void preOrder(Node* root) {
-        if (root != nullptr) {
+    void preOrder(Node *root)
+    {
+        if (root != nullptr)
+        {
             std::cout << root->key << " ";
             preOrder(root->left);
             preOrder(root->right);
@@ -98,21 +127,25 @@ private:
     }
 
 public:
-    AVLTree() {
+    AVLTree()
+    {
         root = nullptr;
     }
 
-    void insert(int key) {
+    void insert(int key)
+    {
         root = insert(root, key);
     }
 
-    void printPreOrder() {
+    void printPreOrder()
+    {
         preOrder(root);
         std::cout << std::endl;
     }
 };
 
-int main() {
+int main()
+{
     AVLTree tree;
     tree.insert(1);
     tree.insert(2);
